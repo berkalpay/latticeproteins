e6ed466d35bfa710dab5c19d55b12969e68578f7
berkalpay <46028827+berkalpay@users.noreply.github.com>
2022-09-09 11:08:29 -0400

    update tests

--- latticeproteins/tests/test_conformations.py
+++ latticeproteins/tests/test_conformations.py
@@ -1,96 +1,15 @@
-"""Tests for conformations module.
+from latticeproteins.core import Conformation
 
-Written by Zach Sailer, 2017.
-"""
-import os, shutil
-import unittest
-from nose import with_setup
-from ..conformations import lattice_contacts, Conformations, ConformationList
+def test_locations():
+    true_locs = {(0, 0), (0, 1), (0, 2), (0, 3), (1, 3), (1, 4),
+                 (2, 4), (2, 3), (2, 2), (2, 1), (1, 1), (0, 1)}
+    assert set(Conformation("UUURURDDDLL").get_locations()) == true_locs
 
-from nose import tools
+def test_overlapping():
+    assert Conformation("UUURURDDDLL").overlapping()
 
-def test_lattice_contacts():
-    seq = "ABCDEFGHIJKLMNOPQR"
-    conf = "URDRUULLLDDDRRDLL"
-    contacts = lattice_contacts(seq, conf)
-    tools.assert_equal(len(contacts), 10)
+def test_non_overlapping():
+    assert not Conformation("UUURURUUR").overlapping()
 
-class TestConformations(unittest.TestCase):
-
-    def setUp(self):
-        self.database = 'database'
-        self.length = 6
-        self.seq = "AHKEDP"
-        self.temp = 1
-
-    def tearDown(self):
-        # Remove a database if successfully created.
-        try:
-            shutil.rmtree("database")
-        except FileNotFoundError:
-            pass
-
-    def test__init__(self):
-        c = Conformations(self.length, self.database)
-        self.assertEqual(os.path.exists(self.database), True)
-        self.assertNotEqual(os.path.exists(self.database), False)
-        self.assertEqual(c._length, self.length)
-
-    def test_fold_sequence(self):
-        c = Conformations(self.length, self.database)
-        minE, conf, partitionsum, folds = c.fold_sequence(self.seq, self.temp)
-        self.assertEqual(type(folds), bool)
-        self.assertEqual(len(conf), self.length-1)
-
-    def test_k_lowest_confs(self):
-        c = Conformations(self.length, self.database)
-        out = c.k_lowest_confs(self.seq, self.temp, 4)
-        # Number of lowest conformations better be 4
-        self.assertEqual(len(out), 4)
-        # Length of conformation string is length -1
-        self.assertEqual(len(out[0]), self.length - 1)
-
-    def test_max_contacts(self):
-        c = Conformations(self.length, self.database)
-        n = c.max_contacts()
-        # Should only see 2 contacts in 6-site lattice protein
-        self.assertEqual(n, 2)
-
-    def test_num_conformations(self):
-        c = Conformations(self.length, self.database)
-        n = c.num_conformations()
-        # Should have 36 conformations for a 6 site lattice protein
-        self.assertEqual(n, 36)
-
-
-class TestConformationList(unittest.TestCase):
-
-    def setUp(self):
-        self.length = 6
-        self.seq = "AHKEDP"
-        self.temp = 1
-        self.confs = ['URRDL', 'UURDD', 'URDRU']
-
-    def test__init__(self):
-        c = ConformationList(self.length, self.confs)
-        self.assertEqual(c._length, self.length)
-
-    def test_fold_sequence(self):
-        c = ConformationList(self.length, self.confs)
-        minE, conf, partitionsum, folds = c.fold_sequence(self.seq, self.temp)
-        self.assertEqual(type(folds), bool)
-        self.assertEqual(len(conf), self.length-1)
-        self.assertEqual(conf, self.confs[1])
-        self.assertGreater(partitionsum, 70.0)
-
-    def test_max_contacts(self):
-        c = ConformationList(self.length, self.confs)
-        n = c.max_contacts()
-        # Should only see 2 contacts in 6-site lattice protein
-        self.assertEqual(n, 2)
-
-    def test_num_conformations(self):
-        c = ConformationList(self.length, self.confs)
-        n = c.num_conformations()
-        # Should have 36 conformations for a 6 site lattice protein
-        self.assertEqual(n, 3)
+def test_contacts():
+    assert len(Conformation("URDRUULLLDDDRRDLL").contacts()) == 10
