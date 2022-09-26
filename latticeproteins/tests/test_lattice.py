57c9016d833f719e4b4f228660b7bf9f44698d1d
berkalpay <46028827+berkalpay@users.noreply.github.com>
2022-09-26 11:07:08 -0400

    align with new code structure, load lattice, round stability, correct bond dir rotation

--- latticeproteins/tests/test_lattice.py
+++ latticeproteins/tests/test_lattice.py
@@ -1,26 +1,25 @@
 from unittest import TestCase
-from latticeproteins.core import Lattice, Protein, Conformation
-from latticeproteins.thermodynamics import stability
+import pickle
+
+from latticeproteins.core import Lattice, Protein
 
 
 def test_num_conformations():
-    assert len(Lattice(L=6).conformations) == 36
+    assert len(list(Lattice(L=6).ensemble.conformations())) == 36
 
 
 class TestFolding(TestCase):
     def setUp(self):
-        self.lattice = Lattice(L=18)
+        #self.lattice = Lattice(L=18)
+        self.lattice = pickle.load(open("../lattice18.pickle", "rb"))  # TODO: standardize path
         self.protein = Protein("CDEFFKKHCIERMFMCYW")
         self.lattice.fold(self.protein)
 
     def test_does_fold(self):
-        conf = self.protein.conformation
-        assert conf and isinstance(conf, Conformation)
+        assert self.protein.native_state()
 
     def test_conformation(self):
-        assert self.protein.conformation == Conformation("URDDLLDRRRRULURDL")
+        assert self.protein.native_state().bond_dirs == "URDRURDDDDLUULLDR"
 
     def test_stability(self):
-        assert stability(self.lattice.energy(self.protein.seq, self.protein.conformation),
-                         sum(self.lattice.all_energies(self.protein.seq)),
-                         1.0) == -1.04
+        assert round(self.protein.stability(temp=1.0), 2) == -1.04
