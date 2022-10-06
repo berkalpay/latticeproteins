2ee07a121def851459da8099c85d5c31cce7c545
berkalpay <46028827+berkalpay@users.noreply.github.com>
2022-10-06 10:58:27 -0400

    change pickled lattice path, convert some assertions to unittest assertions

--- latticeproteins/tests/test_lattice.py
+++ latticeproteins/tests/test_lattice.py
@@ -11,18 +11,18 @@ def test_num_conformations():
 class TestFolding(TestCase):
     def setUp(self):
         #self.lattice = Lattice(L=18)
-        self.lattice = pickle.load(open("../lattice18.pickle", "rb"))  # TODO: standardize path
+        self.lattice = pickle.load(open("lattice18.pickle", "rb"))  # TODO: standardize path
         self.protein = Protein("CDEFFKKHCIERMFMCYW")
         self.lattice.fold(self.protein)
 
     def test_does_fold(self):
-        assert self.protein.native_state
+        self.assertIsNotNone(self.protein.native_state)
 
     def test_conformation(self):
-        assert self.protein.native_state.bond_dirs == "URDRURDDDDLUULLDR"
+        self.assertEqual(self.protein.native_state.bond_dirs, "URDRURDDDDLUULLDR")
 
     def test_stability(self):
-        assert round(self.protein.stability(temp=1.0), 2) == -1.04
+        self.assertEqual(round(self.protein.stability(temp=1.0), 2), -1.04)
 
 
 def test_lattices_equal():
